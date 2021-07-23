0ed534b87e5536f182a35fa2868f360453b22dc2
Parth Kulshreshtha <85359173+Parthkul@users.noreply.github.com>
2021-07-23 09:07:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ setTimeout(() => {
 }, 1000);
 
 document.onkeydown = function (e) {
-    console.log("Key code is : ", e.keyCode);
     if (e.keyCode == 38) {
         dino = document.querySelector(".dino");
         dino.classList.add("animateDino");
